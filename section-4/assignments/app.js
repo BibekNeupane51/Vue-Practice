1d2fb88f22ac3c1458f0e307f3b5c90ee3e7e40b
LEGION5\bibek <neupane.bibek@cloco.co.jp>
2023-04-01 23:44:20 +0545

    nth

--- section-4/assignments/app.js
+++ section-4/assignments/app.js
@@ -2,8 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       inputClass: "",
-      toggleParagraph: false,
+      paragraphVisible: true,
+      inlineClass: "",
     };
+  },
+  computed: {
+    paraClasses() {
+      return {
+        user1: this.inputClass === "user1",
+        user2: this.inputClass === "user2",
+        visible: this.paragraphVisible,
+        hidden: !this.paragraphVisible,
+      };
+    },
+  },
+  methods: {
+    toggleParagraph() {
+      this.paragraphVisible = !this.paragraphVisible;
+    },
   },
 });
 app.mount("#assignment");
